921d466d463b418b5277613ac87ee6cde931a396
ssang <qkqh34k@gmail.com>
2025-08-10 22:40:27 +0900

    캡슐화2

--- chapter01/src/main/java/org/eternity/theater/Audience.java
+++ chapter01/src/main/java/org/eternity/theater/Audience.java
@@ -7,7 +7,15 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L; // 무료 티켓
+        } else {
+            bag.minusAmount(ticket.getFee());
+            bag.setTicket(ticket);
+            return ticket.getFee(); // 유료 티켓
+        }
     }
+
 }
