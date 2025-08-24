2c7ccc6d458d6d515ac4c3dde88cf1756e1d5497
ssang <qkqh34k@gmail.com>
2025-08-24 16:36:27 +0900

    1. 템플릿 메서드 패턴 2. 의존성의 양면성 (컴파일 시간 의존성과 실행 시간 의존성)

--- chapter02/src/main/java/org/eternity/movie/step01/DiscountPolicy.java
+++ chapter02/src/main/java/org/eternity/movie/step01/DiscountPolicy.java
@@ -6,6 +6,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * DiscountPolicy는 할인 여부와 요금 계산에 필요한 전체적인 흐름은 정의하지만
+ * 실제로 요금을 계산 하는 부분은 추상 메서드인 getDiscountAmount 메서드에게 위임한다.
+ *
+ * 이처럼 부모 클래스에 기본적인 알고리즘의 흐름을 구현하고
+ * 중간에 필요한 처리를 자식 클래스에게 위임하는 디자인 패턴을
+ * TEMPLATE METHOD 패턴이라고 부른다.
+ */
 public abstract class DiscountPolicy {
     private List<DiscountCondition> conditions = new ArrayList<>();
 
--- chapter02/src/main/java/org/eternity/movie/step01/Movie.java
+++ chapter02/src/main/java/org/eternity/movie/step01/Movie.java
@@ -10,6 +10,19 @@ public class Movie {
     private Money fee;
     private DiscountPolicy discountPolicy;
 
+    /**
+     * 코드 수준에서 Movie 클래스는 추상클래스(DiscountPolicy)를 의존한다.
+     * 그러나 실행 시점에는 구체 클래스(AmountDiscountPolicy, PercentDiscountPolicy, NoneDiscountPolicy)를 의존하게 된다.
+     *
+     * 이처럼 코드의 의존성과 실행 시점의 의존성은 다를 수 있다.
+     *
+     * 1. 코드의 의존성과 실행 시점의 의존성이 다르면 다를수록 코드를 이 해하기 어려워진다.
+     *  코드를 이해하기 위해서는 코드뿐만 아니라 객체를 생성하고 연결하는 부분을 찾아야 하기 때문이다.
+     * 2. 반면 코드의 의존성과 실행 시점의 의존성이 다르면 다를수록 코드는 더 유연해지고 확장 가능해진다.
+     *
+     * 이와 같은 의존성의 양면성은 설계가 트레이드오프의 산물이라는 사실을 잘 보여준다.
+     *
+     */
     public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
         this.title = title;
         this.runningTime = runningTime;
