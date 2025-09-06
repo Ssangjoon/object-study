229704fa938c1f4005445e53129c687750e43e5e
ssang <qkqh34k@gmail.com>
2025-09-06 22:33:11 +0900

    .

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+
+plugins {
+    java
+    kotlin("jvm") version "1.9.23" apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("java")
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    dependencies {
+    }
+}
+
+project(":study:object") {
+    dependencies {
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "study-practice"
+
+include("study:object")
+include("study:head-first-design-pattern")
+include("practice:spring-transaction")
+include("practice:git-rebase")
+include("practice:modern-java")
