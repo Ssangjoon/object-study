55fb2d5e6da3c01b1bdf684ff6320066f8d3097c
ssang <qkqh34k@gmail.com>
2025-09-08 22:20:54 +0900

    pr rebase test commit 1

--- practice/modern-java/src/main/java/LambdaPractice.java
+++ practice/modern-java/src/main/java/LambdaPractice.java
@@ -2,4 +2,5 @@ public class LambdaPractice {
     String commit1 = "Initial commit";
     String commit2 = "Added feature A";
     String commit3 = "Fixed bug in feature A";
+    String commit4 = "Improved performance";
 }
